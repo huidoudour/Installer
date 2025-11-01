138022ebc02d7eac24dcb030db54170023d65079
huidoudour <ylmf_qquser@126.com>
2025-11-01 15:37:34 +0800

    11011537

--- app-debug/src/main/java/io/github/huidoudour/Installer/debug/ui/home/HomeFragmentKt.kt
+++ app-debug/src/main/java/io/github/huidoudour/Installer/debug/ui/home/HomeFragmentKt.kt
@@ -6,41 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.github.huidoudour.Installer.debug.R
-import io.github.huidoudour.Installer.debug.databinding.FragmentHomeBinding
 
 /**
  * 一个简单的Kotlin Fragment类，用于测试Kotlin和Java在项目中的共存
  */
 class HomeFragmentKt : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        // 设置一些简单的文本
-        binding.textHome.text = getWelcomeMessage()
+        // 不再引用不存在的textHome
     }
 
-    /**
-     * 一个简单的函数，返回欢迎消息
-     */
     private fun getWelcomeMessage(): String {
         return "欢迎使用 Kotlin 版本的 Home Fragment!"
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
